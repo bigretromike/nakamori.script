375d2adbc6d4a840501be81c090d225fde405aa0
bigretromike <bigretromike@users.noreply.github.com>
2018-09-08 23:36:15 +0200

    small fix;

--- default.py
+++ default.py
@@ -10,19 +10,19 @@ import xbmcaddon
 
 class Main:
     def __init__(self):
-        self.params = nt.parse_parameters(sys.argv[1])
-
-        if not self.params:
-            pass
-        else:
-            if self.params['info'] == 'calendar':
-                calendar.open_calendar(date=self.params.get('date', 0), starting_item=self.params.get('page', 0))
-            elif self.params['info'] == 'wizard':
-                wizard.open_wizard()
-            elif self.params['info'] == 'clearcache':
-                calendar.clear_cache()
-            elif self.params['info'] == 'settings':
-                xbmcaddon.Addon(id='script.module.nakamori').openSettings()
+        if len(sys.argv) > 1:
+            self.params = nt.parse_parameters(sys.argv[1])
+            if not self.params:
+                pass
+            else:
+                if self.params['info'] == 'calendar':
+                    calendar.open_calendar(date=self.params.get('date', 0), starting_item=self.params.get('page', 0))
+                elif self.params['info'] == 'wizard':
+                    wizard.open_wizard()
+                elif self.params['info'] == 'clearcache':
+                    calendar.clear_cache()
+                elif self.params['info'] == 'settings':
+                    xbmcaddon.Addon(id='script.module.nakamori').openSettings()
 
 
 if __name__ == "__main__":

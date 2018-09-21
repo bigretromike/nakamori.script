cf3b039c7a8b6c741c2632fca26a950fef44217f
da3dsoul <da3dsoul@gmail.com>
2018-09-21 03:09:37 -0400

    Remove the plugin.py icon. Make default.py give a nice menu. I didn't remove plugin routing, as it's really cool, but it's not useful in nakamori script

--- default.py
+++ default.py
@@ -1,34 +1,83 @@
 import sys
 
-import lib.windows.calendar as calendar
-import lib.windows.information as information
-import lib.windows.wizard as wizard
+import lib.windows.calendar as _calendar
+import lib.windows.information as _information
+import lib.windows.wizard as _wizard
 import lib.nakamoritools as nt
 
+import xbmcgui
 from xbmcaddon import Addon
-from xbmcgui import Dialog
 
 
-class Main:
-    def __init__(self):
-        if len(sys.argv) > 1:
-            self.params = nt.parse_parameters(sys.argv[1])
-            if not self.params:
-                pass
-            else:
-                if self.params['info'] == 'calendar':
-                    calendar.open_calendar(date=self.params.get('date', 0), starting_item=self.params.get('page', 0))
-                elif self.params['info'] == 'wizard':
-                    wizard.open_wizard()
-                elif self.params['info'] == 'clearcache':
-                    calendar.clear_cache()
-                elif self.params['info'] == 'information':
-                    information.open_information()
-                elif self.params['info'] == 'settings':
-                    Addon(id='script.module.nakamori').openSettings()
+_addon = Addon(id='script.module.nakamori')
+
+
+def main():
+    if len(sys.argv) > 1:
+        params = nt.parse_parameters(sys.argv[1])
+        if not params:
+            pass
         else:
-            Dialog().ok('Nakamori script', 'Baka!')
+            if params['info'] == 'calendar':
+                _calendar.open_calendar(date=params.get('date', 0), starting_item=params.get('page', 0))
+            elif params['info'] == 'wizard':
+                _wizard.open_wizard()
+            elif params['info'] == 'clearcache':
+                _calendar.clear_cache()
+            elif params['info'] == 'information':
+                _information.open_information()
+            elif params['info'] == 'settings':
+                _addon.openSettings()
+            else:
+                root()
+    else:
+        root()
+            
+
+def root():
+    items = [
+        ("Wizard", (wizard, [])),
+        ("Calendar", (calendar, ['0', '0'])),
+        ("Calendar (Add 14 Days)", (calendar, ['0', '14'])),
+        ("Information", (information, [])),
+        ("Clear-cache", (clearcache, [])),
+        ("Settings", (settings, []))
+    ]
+
+    options = []
+    for item in items:
+        options.append(item[0])
+
+    result = xbmcgui.Dialog().select('Nakamori Script', options)
+    if result >= 0:
+        action, args = items[result][1]
+        action(*args)
+
+
+def calendar(when, page):
+    _calendar.open_calendar(date=when, starting_item=page)
+    return True
+
+
+def wizard():
+    _wizard.open_wizard()
+    pass
+
+
+def clearcache():
+    _calendar.clear_cache()
+    pass
+
+
+def information():
+    _information.open_information()
+    pass
+
+
+def settings():
+    _addon.openSettings()
+    pass
 
 
 if __name__ == "__main__":
-    Main()
+    main()

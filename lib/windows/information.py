adbdb222a85a39b194d357b602110534fc30c5ce
da3dsoul <da3dsoul@gmail.com>
2019-04-03 07:12:10 -0400

    Improvements to Dialogs. What's New Dialog revamp

--- lib/windows/information.py
+++ lib/windows/information.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
-import os
+from collections import defaultdict
+from distutils.version import LooseVersion
 
 import xbmcgui
-import xbmcaddon
-import xml.etree.ElementTree as ET
+
+from nakamori_utils.globalvars import *
+
+from proxy.python_version_proxy import python_proxy as pyproxy
 
 ADDON = xbmcaddon.Addon(id='script.module.nakamori')
 CWD = ADDON.getAddonInfo('path').decode('utf-8')
@@ -20,17 +23,18 @@ class Information(xbmcgui.WindowXMLDialog):
         self.window_type = 'window'
 
     def onInit(self):
-        _close = self.getControl(CLOSE_BUTTON)
-        _textbox = self.getControl(CONTENT_TEXTBOX)
-        tree = ET.parse(os.path.join(xbmcaddon.Addon(id='plugin.video.nakamori').getAddonInfo('path').decode('utf-8'),
-                                     'addon.xml'))
-        root = tree.getroot()
-        news = root.findall('.//*[@point="xbmc.addon.metadata"]/news')
-        _textbox.setText(news[0].text)
-        self.setFocus(_close)
+        _title = self.getControl(1)
+        assert isinstance(_title, xbmcgui.ControlLabel)
+        _title.setLabel('What\'s New')
+
+        changelog_text = get_changelog_text()
+
+        _textbox = self.getControl(5)
+        assert isinstance(_textbox, xbmcgui.ControlTextBox)
+        _textbox.setText(changelog_text)
 
     def onAction(self, action):
-        if action == ACTION_PREVIOUS_MENU or action == ACTION_NAV_BACK:
+        if action == ACTION_PREVIOUS_MENU or action == ACTION_NAV_BACK or action:
             self.close()
 
     def onControl(self, control):
@@ -43,7 +47,55 @@ class Information(xbmcgui.WindowXMLDialog):
         pass
 
 
+def get_changelog_text():
+    # Populate the changelog internally
+    changelog_path = os.path.join(pyproxy.decode(xbmcaddon.Addon(id='plugin.video.nakamori').getAddonInfo('path')),
+                                  'changelog.txt')
+    fstream = open(changelog_path, 'r')
+    changelog = defaultdict(list)
+    current_version = None
+    for line in fstream.readlines():
+        try:
+            line = line.strip()
+            if line == '':
+                continue
+            if line.startswith('#'):
+                continue
+            if line.startswith('!--'):
+                try:
+                    current_version = LooseVersion(line.replace('!--', '').strip())
+                    # current line is version so go to next line
+                    continue
+                except:
+                    pass
+            if current_version is None:
+                continue
+            changelog[current_version.vstring].append(line)
+        except Exception as e:
+            pass
+    changelog.default_factory = None
+
+    # build the text based on previous version.
+    # This is important, as someone might open kodi for the first time in a while and skip several versions
+    previous_version = LooseVersion(plugin_addon.getSetting('version'))
+    versions = []
+    for k, v in changelog.items():
+        if LooseVersion(k) > previous_version:
+            versions.append((k, v))
+    versions.sort(reverse=True)
+
+    changelog_text = ''
+    for k, v in versions:
+        changelog_text += 'Version ' + k
+        for line in v:
+            changelog_text += '\n- ' + line
+        changelog_text += '\n'
+
+    return changelog_text
+
+
 def open_information():
-    ui = Information('information.xml', CWD, 'default', '1080i')
+    ui = Information('DialogTextViewer.xml', os.getcwd(), 'Default', '1080i')
     ui.doModal()
     del ui
+    plugin_addon.setSetting('version', plugin_addon.getAddonInfo('version'))
